ecdc530429433021ee00eb036b9fac925c97617e
Ricardo Tulio <ledo.tulio@gmail.com>
2021-11-15 21:49:59 +0000

    components: allow cards to be edited

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-const Card = ({ title, description }) => (
+const Card = ({ isBeingEdited = false, onChange, title, description }) => (
   <div className={styles.card}>
     <div>
-      <h3>{ title }</h3>
+      {!isBeingEdited ? <h3>{ title }</h3> : <input type='text' value={ title } onChange={ onChange } />} 
     </div>
     <div>
       <p>{ description }</p>
     </div>
     <div>
       <a href='#'>edit</a>
-      <a href='#'>delete</a>
+      <a href='#' onClick={ onDeleteClick }>delete</a>
     </div>
   </div>
 )
