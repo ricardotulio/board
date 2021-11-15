385d4e8dfeb2da3eafaa9516393615a362ca7aca
Ricardo Tulio <ledo.tulio@gmail.com>
2021-11-15 12:29:06 +0000

    containers: refact BoardContainer to use pipe function
    Use pipe function to cascade the functions instead of just add one after
    another.

--- src/containers/BoardContainer/BoardContainer.jsx
+++ src/containers/BoardContainer/BoardContainer.jsx
@@ -1,29 +1,28 @@
 import React, { useState } from 'react'
+import { pipe, prop, append, __ } from 'ramda'
 import Board from '../../components/Board'
 import Modal from '../../components/Modal'
 import ColumnContainer from '../ColumnContainer'
 import ColumnFormContainer from '../ColumnFormContainer'
 
 const BoardContainer = ({ boardService }) => {
-  const [ board, setBoard ] = useState(boardService.fetchBoard())
-  const [ modalState, setModalState ] = useState(false) 
-  
+  const board = boardService.fetchBoard()
+
+  const [ columns, setColumns ] = useState(prop('columns', board))
+  const [ modalState, setModalState ] = useState(false)
+
   const showModal = () => setModalState(true)
   const hideModal = () => setModalState(false)
   
-  const addColumn = column => {
-    const { columns } = board
-  
-    setBoard({
-      columns: [ ...columns, column ],
-    })
-  
-    hideModal()
-  }
+  const addColumn = pipe(
+    append(__, columns),
+    setColumns,
+    hideModal,
+  )
 
   return (
     <Board>
-      {board.columns.map(
+      {columns.map(
         (column, index) => (
           <ColumnContainer column={ column } />
         )
