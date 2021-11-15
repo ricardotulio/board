b695573a94782adc49298ae6c86001a5c131e1b0
Ricardo Tulio <ledo.tulio@gmail.com>
2021-11-15 21:49:34 +0000

    components: remove unsed imports from Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import CardTitle from './CardTitle'
-import CardDescription from './CardDescription'
-import CardActions from './CardActions'
 import styles from './styles.module.css'
 
 const Card = ({ title, description }) => (
