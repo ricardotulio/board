6143240c63c0d1094103111662326caee2e47420
Ricardo Tulio <ledo.tulio@gmail.com>
2021-11-15 12:11:44 +0000

    containers: move board state management to BoardContainer

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import { useState } from 'react'
 import BoardService from './services/board'
 import BoardContainer from './containers/BoardContainer'
-import ColumnFormContainer from './containers/ColumnFormContainer'
-import Modal from './components/Modal'
 
-const App = () => {
-  const [ board, setBoard ] = useState(BoardService.fetchBoard())
-  const [ modalState, setModalState ] = useState(false) 
-
-  console.log(board.columns)
-
-  const showModal = () => setModalState(true)
-  const hideModal = () => setModalState(false)
-
-  const addColumn = column => {
-    const { columns } = board
-
-    setBoard({
-      columns: [ ...columns, column ],
-    })
-
-    hideModal()
-  }
-
-  return (
-    <div>
-      <BoardContainer board={ board } />
-      <a href='#' onClick={ showModal }>add column</a>
-
-      <Modal active={ modalState }>
-        <ColumnFormContainer handleSubmit={ addColumn } />
-      </Modal>
-    </div>
-  )
-}
+const App = () => (
+  <BoardContainer boardService={ BoardService } />
+)
 
 export default App
--- src/containers/BoardContainer/BoardContainer.jsx
+++ src/containers/BoardContainer/BoardContainer.jsx
@@ -1,15 +1,41 @@
 import React, { useState } from 'react'
 import Board from '../../components/Board'
+import Modal from '../../components/Modal'
 import ColumnContainer from '../ColumnContainer'
+import ColumnFormContainer from '../ColumnFormContainer'
 
-const BoardContainer = ({ board }) => (
-  <Board>
-    {board.columns.map(
-      (column, index) => (
-        <ColumnContainer column={ column } />
-      )
-    )}
-  </Board>
-)
+const BoardContainer = ({ boardService }) => {
+  const [ board, setBoard ] = useState(boardService.fetchBoard())
+  const [ modalState, setModalState ] = useState(false) 
+  
+  const showModal = () => setModalState(true)
+  const hideModal = () => setModalState(false)
+  
+  const addColumn = column => {
+    const { columns } = board
+  
+    setBoard({
+      columns: [ ...columns, column ],
+    })
+  
+    hideModal()
+  }
+
+  return (
+    <Board>
+      {board.columns.map(
+        (column, index) => (
+          <ColumnContainer column={ column } />
+        )
+      )}
+
+      <a href='#' onClick={ showModal }>add column</a>
+
+      <Modal active={ modalState }>
+        <ColumnFormContainer handleSubmit={ addColumn } />
+      </Modal>
+    </Board>
+  )
+}
 
 export default BoardContainer
